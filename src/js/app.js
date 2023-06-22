28f496e653b7fe80370d0105ed2573c9e79bd0a6
Diana Chacón <diianachm1802@gmail.com>
2023-06-22 04:24:46 +0000

    Conditional Profile Card Generator v0.1.0

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,33 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name == null ? "Diana" : variables.name} ${
+    variables.lastname == null ? "Chacón" : variables.lastname
+  }</h1>
+          <h2>${variables.role == null ? "Web Developer" : variables.role}</h2>
+          <h3>${variables.city == null ? "Cartago" : variables.city}, ${
+    variables.country == null ? "Costa Rica" : variables.country
+  }</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href=https://twitter.com/${
+              variables.twitter == null ? "dianademoon" : variables.twitter
+            }><i class="fab fa-twitter"></i></a></li>
+
+            <li><a href=https://github.com/${
+              variables.github == null ? "Viridian-a" : variables.github
+            }
+            ><i class="fab fa-github"></i></a></li>   
+
+            <li><a href=https://linkedin.com/in/
+            ${
+              variables.linkedin == null
+                ? "diana-chacón-montero-ab5529202"
+                : variables.linkedin
+            }><i class="fab fa-linkedin"></i></a></li>
+
+            <li><a href=https://instagram.com/${
+              variables.instagram == null ? "dianainred" : variables.instagram
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,14 +73,15 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=872&q=80",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://avatars.githubusercontent.com/u/128646689?v=4",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-right",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "Viridian-a",
     linkedin: null,
     instagram: null,
     name: null,
